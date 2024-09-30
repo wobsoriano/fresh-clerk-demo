066644b675295dd164eb868ec97fefb9fce6cf84
wobsoriano <sorianorobertc@gmail.com>
2024-09-29 23:39:27 -0700

    clean up classes

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 class="text-2xl font-bold mb-5">Welcome</h1>
-      <p class="mb-5">
+      <p>
         A demo for Fresh Clerk. Go ahead and sign up or sign in!
       </p>
     </>
